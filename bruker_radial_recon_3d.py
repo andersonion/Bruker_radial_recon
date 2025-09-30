c34ece12b531f6a1b7801fc90cd239dbad716b72
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-09-30 16:14:27 -0400

    Debugging 3D recon code, closing in on production (autotune) mode.

--- bruker_radial_recon_3d.py
+++ bruker_radial_recon_3d.py
@@ -501,12 +501,12 @@ def main():
 
 
 	# Optional clip (broadcast-safe)
-	if args.clip_pi:
-		r = np.linalg.norm(coords, axis=1, keepdims=True)       # (M,1)
-		s = np.minimum(1.0, np.pi / (r + 1e-12))                # (M,1) scale ≤ 1
-		nclip = int((r > np.pi).sum())
-		coords *= s                                             # (M,3) ← (M,1) broadcast
-		print(f"[pre] Clipped |k| to ≤ π for {nclip}/{coords.shape[0]} samples.")
+    if args.clip_pi:
+    	r = np.linalg.norm(coords, axis=1, keepdims=True)       # (M,1)
+    	s = np.minimum(1.0, np.pi / (r + 1e-12))                # (M,1) scale ≤ 1
+    	nclip = int((r > np.pi).sum())
+    	coords *= s                                             # (M,3) ← (M,1) broadcast
+    	print(f"[pre] Clipped |k| to ≤ π for {nclip}/{coords.shape[0]} samples.")
 
 
     # ----- PROD mode: fast tuner then final -----
